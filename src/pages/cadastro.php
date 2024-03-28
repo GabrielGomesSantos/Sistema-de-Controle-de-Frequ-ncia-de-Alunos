c5d17dd81ed5440086ee41f30ed34cac41b4de89
Peter William <wpeterwill@gmail.com>
2024-03-28 17:07:16 -0300

    CSS
    css das paginas

--- src/pages/cadastro.php
+++ src/pages/cadastro.php
@@ -5,25 +5,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..\..\assets\css\style_cadastro.css">
+
+    <?php include ('links_css.php') ?>
+
+
+
     <title>Document</title>
 </head>
+<header>
+    <img src="https://www.fiemg.com.br/senai/wp-content/uploads/sites/12/2023/02/Branco_TAG_SENAI_.png" alt="">
+</header>
 <body>
 
-    <h1>Cadastrar</h1>
-
-        <form action="salvar_cadastro.php" method="post">
-
-            <label for="usuario">Usuario:</label><br>
-            <input type="text" id="usuario" name="usuario" required><br>
-
-            <label for="password">Senha:</label><br>
-            <input type="text" id="password" name="password" required><br>
-
-            <label for="tipo">Tipo:</label><br>
-            <input type="text" id="tipo" name="tipo" required><br><br>
-
-            <input type="submit" value="Enviar">
-        </form>
     
+    <div class="content">
+        <div class="formulario">
+
+        <header class="header1"></header>
+        <form action="salvar_cadastro.php" method="post">
+            
+            <h1>Cadastrar</h1>
+            <label for="usuario">Usuario:</label>
+            <input  class="inputtext" type="text" id="usuario" name="usuario" required>
+
+            <label for="password">Senha:</label>
+            <input class="inputtext" type="text" id="password" name="password" required>
+
+            <label for="tipo">Tipo:</label>
+            
+            <select class="inputtext" id="tipo" name="tipo"  required>
+            <option value="1">Secretaria - Senai</option>
+            <option value="2">Empresa</option>
+            <div class="buttonsubmit">
+                <input class="submit"type="submit" value="Enviar">
+            </div>
+        </form>
+        </div>
+    </div>
 </body>
 </html>
