1755b29ce9829682dbfc1fe900210379be58c8c0
Yan <manciniyan766@gmail.com>
2024-03-28 14:56:58 -0300

    Frequencia

---
+++ src/pages/conexao.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "frequencia";
+
+// Cria a conexão
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// Define o conjunto de caracteres para utf8
+mysqli_set_charset($conn, "utf8");
+?>
