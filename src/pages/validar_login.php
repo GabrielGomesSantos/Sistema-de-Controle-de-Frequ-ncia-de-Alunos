3434038f646bfc2a96c50ddcc5ba7a296241d41c
Gabriel Gomes <gabrielgones5@gmail.com>
2024-04-24 16:22:19 -0300

    a

--- src/pages/validar_login.php
+++ src/pages/validar_login.php
@@ -27,15 +27,47 @@
             header('Location: dashboard.php');
             exit();
 
-        } else {
+        } else { ?>
+            
 
-            echo "<script> 
-                if (confirm('Usuario e/ou Senha invalidos!')) {
-                    window.location.href = '../../index.php';
-                } else {
-                    window.location.href = '../../index.php';
-                }
-                </script>";
+            <!doctype html>
+            <html lang="en">
+            <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Bootstrap demo</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+            </head>
+            <body>
+
+            <!-- Modal -->
+            <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Erro de autenticação</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Usuário ou senha inválidos!
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                </div>
+                </div>
+            </div>
+            </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            <script>
+            var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+            myModal.show();
+            </script>
+            </body>
+            </html>
+
+            
+        <?php
         }
     } else {
         header('Location: ../../index.php');
